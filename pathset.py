009bdc507fd7cc5fc9b0d2974d932a628099dbfe
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2019-10-14 15:51:06 +0100

    Added iter_files to loop over DataFrame and find the rght filestems. Code works and prints out xml objects

--- pathset.py
+++ pathset.py
@@ -11,12 +11,13 @@
 #
 
 ######## Imports ##############
-import xmp.etree.ElementTree as ET # ElementTree is a standard (as of python 2.5) library which we can use to parse XML.
+import xml.etree.ElementTree as ET # ElementTree is a standard (as of python 2.5) library which we can use to parse XML.
                                    # N.B ET is NOT secure against "malicous XML" however as we are only intersted in very simple XML this shouldnt be an issue
 import pandas as pd
 import os
 from pathlib import Path
 from shutil import copy
+import glob
 ###############################
 
 class Setter:
@@ -32,7 +33,7 @@ class Setter:
         self.df = df_in[df_in.STAT == station]
         if odir == None:
             self.opath = os.getcwd() # Gets current working directory and stores is as a Path
-
+        self.station = station
         self.ddir = ddir # Data directory (hopefully)
 
     def get_mts(self,phase,fileID):
@@ -40,9 +41,9 @@ class Setter:
            Phase [str] - SKS or SKKS (as my sheba runs are split by phase)
            fileID [str] - file name (.mts is appended here)
         '''
-        mts = '~/Shear_Wave_Splitting/Sheba/Runs/{}/{}/{}/{}.mts'.format(self.ddir,self.station,phase)
+        mts = '{}/{}/{}/{}.mts'.format(self.ddir,self.station,phase,fileID)
         xml = ET.parse(mts) # Parse the xml file (output from sheba as .mts)
-
+        print(xml) # Just print this for now as a test that it works
 
     def get_sac(self,phase,fileID):
         '''Function to copy data to the local data directory "data" if it is not already there (mainly useful for test/first runs).
@@ -52,9 +53,21 @@ class Setter:
         for comp in ['E','N','Z']:
             f = Path('{}/data/{}.BH{}'.format(self.opath,fileID,comp))
             if f.is_file():
-                print('./data/{}.BH{} exists, not copying'.format(fileID,comp')
+                print('./data/{}.BH{} exists, not copying'.format(fileID,comp))
             else:
                 print('File not found, copying from Sheba Run Dir E_pacific if possible')
-                file = '~/Shear_Wave_Splitting/Sheba/Runs/{}/{}/{}/{}.BH{}'.format(self.ddir,self.station,phase,comp)
+                file = '{}/{}/{}/{}.BH{}'.format(self.ddir,self.station,phase,fileID,comp)
                 dst = '{}/data/{}.BH{}'.format(self.opath,fileID,comp)
                 p = copy(file, dst)
+
+    def iter_files(self,phases=['SKS','SKKS']):
+        '''
+        Function to iterate over the DataFrame and construct the XML we need
+        Phases [list] - list of phase codes to iterate over (assuming each row in the DataFame corresponds to all phases)
+        '''
+        for i, row in self.df.iterrows():
+            for ph in phases:
+                f = '{}/{}/{}/{}_{}_{}??_{}.mts'.format(self.ddir,row.STAT,ph,row.STAT,row.DATE,row.TIME,ph)
+                fileID = glob.glob(f)[0].strip('.mts').split('/')[-1] # Strip out .mts and split by '/', select end to get filestem
+                self.get_sac(ph,fileID)
+                self.get_mts(ph,fileID)
