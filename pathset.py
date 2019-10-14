18a57defeaf89f21562bfd77f998febec19f377f
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2019-10-14 14:36:07 +0100

    Created Setter Class and get_mts function

--- pathset.py
+++ pathset.py
@@ -13,7 +13,14 @@
 ######## Imports ##############
 import xmp.etree.ElementTree as ET # ElementTree is a standard (as of python 2.5) library which we can use to parse XML.
                                    # N.B ET is NOT secure against "malicous XML" however as we are only intersted in very simple XML this shouldnt be an issue
+import pandas as pd
+
+###############################
 
 class Setter:
-
-    def __init__():
+    """A class to hold the metadata for the run (rdir, station? [for now], outdir etc. ) and fucntions
+    to parse/generate the XML needed for the PathSet file
+    Inputs: df [obj] - a DataFrame of the relevent .pairs file (Setter will select rows from the relevent station)
+            station [str] - the station code for the station[s] we want to include data for (starting with a single station)
+    """
+    def __init__(df_in,station,rdir):
