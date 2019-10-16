89e57b1ef51bc45949857753e38ed441c05ea795
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2019-10-16 12:11:48 +0100

    Added a new function domain2operator to construct operator XML. Added readin f of Model.xml to __init__

--- pathset.py
+++ pathset.py
@@ -11,8 +11,10 @@
 #
 
 ######## Imports ##############
-import xml.etree.ElementTree as ET # ElementTree is a standard (as of python 2.5) library which we can use to parse XML.
+from xml.etree import ElementTree # ElementTree is a standard (as of python 2.5) library which we can use to parse XML.
                                    # N.B ET is NOT secure against "malicous XML" however as we are only intersted in very simple XML this shouldnt be an issue
+
+from xml.dom import minidom
 import pandas as pd
 import os
 from pathlib import Path
@@ -21,8 +23,6 @@ import glob
 from calc_aoi import slw2aoi
 import numpy as np
 ###############################
-
-
 
 def calc_dist(phase,evdp,depth,h):
     '''
@@ -35,7 +35,6 @@ def calc_dist(phase,evdp,depth,h):
     '''
 
     aoi = slw2aoi(depth,evdp,110,phase) # Calculate ray param and then incidence angle
-    print(aoi)
     dist = h / np.cos(np.radians(aoi))
     return dist
 
@@ -55,33 +54,58 @@ class Setter:
         self.station = station
         self.ddir = ddir # Data directory (hopefully)
         self.dom_h = 250 # [km] height of the domains (fixed for UM and D'' for now!)
+        self.xmlns = {'mtsML':'http://www1.gly.bris.ac.uk/cetsei/xml/MatisseML/'} # Dict with the Matisse XML namespace (for easy ref)
+        ## Read Model.xml (assumed to be in working directory because why wouldnt it?)
+        root = ElementTree.parse('Model.xml').getroot()
+        self.model = root.find('MtsML:model',self.xmlns)
+        model_name = model[0].text
+        print('Using model named ... {}'.format(model_name))
 
 
-    def get_mts(self,phase,fileID):
+    def get_mts(self,phase):
         '''Function to get the .mts file for a phase and read in the xml.
            Phase [str] - SKS or SKKS (as my sheba runs are split by phase)
-           fileID [str] - file name (.mts is appended here)
+           fileID [str] - file name (.mts is appended here) [ now an attribute of Setter class]
         '''
-        mts = '{}/{}/{}/{}.mts'.format(self.ddir,self.station,phase,fileID)
-        xml = ET.parse(mts) # Parse the xml file (output from sheba as .mts)
-        print(xml) # Just print this for now as a test that it works
+        mts = '{}/{}/{}/{}.mts'.format(self.ddir,self.station,phase,self.fileID)
+        xml = ElementTree.parse(mts) # Parse the xml file (output from sheba as .mts)
+        data_element = xml.getroot() # Gets the root element of the XML. In this case (the .mts) this is the tag <data> which we want to inject into the
+                                     # the bigger Pathset XML file
+        return data_element
 
-    def get_sac(self,phase,fileID):
+    def get_sac(self,phase):
         '''Function to copy data to the local data directory "data" if it is not already there (mainly useful for test/first runs).
            Phase [str] - SKS or SKKS (as my sheba runs are split by phase)
-           fileID [str] - file name (.mts is appended here)
+           fileID [str] - file name (.mts is appended here) (now an attribute)
         '''
         for comp in ['E','N','Z']:
-            f = Path('{}/data/{}.BH{}'.format(self.opath,fileID,comp))
+            f = Path('{}/data/{}.BH{}'.format(self.opath,self.fileID,comp))
             if f.is_file():
-                print('./data/{}.BH{} exists, not copying'.format(fileID,comp))
+                print('./data/{}.BH{} exists, not copying'.format(self.fileID,comp))
             else:
                 print('File not found, copying from Sheba Run Dir E_pacific if possible')
-                file = '{}/{}/{}/{}.BH{}'.format(self.ddir,self.station,phase,fileID,comp)
-                dst = '{}/data/{}.BH{}'.format(self.opath,fileID,comp)
+                file = '{}/{}/{}/{}.BH{}'.format(self.ddir,self.station,phase,self.fileID,comp)
+                dst = '{}/data/{}.BH{}'.format(self.opath,self.fileID,comp)
                 p = copy(file, dst)
 
-
+    def domain2operator(self,domain,phase):
+        '''Function to read model.xml and extract the required domain
+        Input -------
+        domain [str] - the domain name (tag <domain_uid>) for the domain we want to find and "cast" as the operator
+        '''
+        domains = self.model.findall('mtsML:domain',self.xmlns)
+        for domain in domains:
+            uid = domain.find('mtsML:domain_uid',self.xmlns).text
+            if uid == domain:
+                print('Domain Found')
+                if uid.split('_')[0] == 'Lower':
+                    # Domains starting with Lower are at CMB. So depth == 2890 km
+                    dep = 2890. # Approx depth of CMB [km]
+                elif uid.split('_')[0] == 'Upper':
+                    dep = 250. # Depth of upper domain (keeping domain the same thickness for now)
+                else:
+                    raise NameError('Domain is incorreclty named. Should be either "Upper" or "Lower".')
+                d_len = calc_dist(phase,self.evdp,dep,self.dom_h) # Calculate path length in domain
 
     def iter_files(self,phases=['SKS','SKKS']):
         '''
@@ -89,8 +113,15 @@ class Setter:
         Phases [list] - list of phase codes to iterate over (assuming each row in the DataFame corresponds to all phases)
         '''
         for i, row in self.df.iterrows():
+            self.evdp = row.EVDP # read out event depth [km]. Attrib as needed for path length calcs.
+            evla = row.EVLA
+            evlo = row.EVLO
+            stla = row.STLA
+            stlo = row.STLO
+            stat = row.STAT
             for ph in phases:
-                f = '{}/{}/{}/{}_{}_{}??_{}.mts'.format(self.ddir,row.STAT,ph,row.STAT,row.DATE,row.TIME,ph)
-                fileID = glob.glob(f)[0].strip('.mts').split('/')[-1] # Strip out .mts and split by '/', select end to get filestem
-                self.get_sac(ph,fileID)
-                self.get_mts(ph,fileID)
+                f = '{}/{}/{}/{}_{}_{}??_{}.mts'.format(self.ddir,stat,ph,stat,row.DATE,row.TIME,ph)
+                self.fileID = glob.glob(f)[0].strip('.mts').split('/')[-1] # Strip out .mts and split by '/', select end to get filestem
+                self.get_sac(ph)
+
+                data = self.get_mts(ph)
