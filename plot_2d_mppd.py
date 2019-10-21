2d47040e3cb2e09206ac061a4876dbc8bf363f34
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2019-10-21 16:44:43 +0100

    Started new function to make prettier/ more functional versional of the output plots JW's matlab scripts produce

---
+++ plot_2d_mppd.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+##############################
+#   Program: plot_2d_mppd.py
+#
+##############################
+#   (C) J. Asplet
+##############################
+#   This program is designed to gather to parse and them plot 2d mppd's (mutli-parameter probability density) output from Matisse.  
+
+######## Imports ##############
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+###############################
