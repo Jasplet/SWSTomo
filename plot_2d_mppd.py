bba22f38ca1454a7f945b732e0a51609ddda72bb
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2019-10-21 17:51:50 +0100

    Started porting JWs base I/O of .p and .xy file

--- plot_2d_mppd.py
+++ plot_2d_mppd.py
@@ -21,13 +21,41 @@ def _plot_1d_ppm(ax,V,P,dompam,pthresh=0.95):
     '''
     Plot a 1-D marginal on the given axes object and returns the axes object (because I am not a bastard)
     ==== Input ====
+    ax - [obj].   A matplotlib pyplot axes object (ideally the one you want the 1d PPM to be plotted on)
     V - [array].  Values of the given parameter to plot the 1-D marginal for (e.g. Gamma for Domain A)
-    P - [array]. Likelihood for the given set of values
+    P - [array].  Likelihood for the given set of values
     pthresh - Probability threshold (defualt of 0.95 taking from Wookey's matlab function)
     '''
 
 def _plot_1d_mppd(ax):
     '''
-    Plot 
+    Copied from JW's matlab scripts, iterates over variables and plots 1d_ppm's
     '''
-def _plot_2d_mppd(ax):
+
+def plot_2d_mppd(i):
+    '''
+    plot the 2-D mppd on
+    i - [str]. A 3 digits code ('001') that corresponds to the MPPD that we want to plot
+    '''
+    #Whilst testing
+    i = '001'
+    # Remove line above when devel is done
+    P = np.loadtxt('MTS_2D_MPPD.{}.p'.format(i),skiprows=1) # Skip header line
+    XY = np.loadtxt('MTS_2D_MPPD.{}.xy'.format(i)) # Two-row file containing X and Y parameters
+    x = XY[0] # array of X-parameters
+    y = XY[1] # array of Y-parameters
+    # Get Header of .p file for domain labels
+    with open(''MTS_2D_MPPD.{}.p'.format(i)','r') as reader:
+        h = reader.readine().strip('%') # read header line and get rid of that pesky % symbol
+        dps = h.split('-')
+        dp_x = doms[0] #Domain & Parameter (should be Domain:Par) in the X dir
+        dp_y = doms[1]
+
+    (X,Y) = np.meshgrid()
+
+if __name == "__main__":
+    # If this script is being run from the command line
+
+    # Do stuff (plotting mainly)
+
+# EOF
