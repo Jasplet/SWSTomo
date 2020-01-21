dd1e83b1be69f3e9cae501c7e6c52e367497434f
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2020-01-21 15:46:51 +0000

    Made a quick login script to connect to bluepebble

---
+++ bp-connect.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# Script to login to bp1-login01b.acrc.bris.ac.uk
+# because I am a) lazy and b) forget what the bp1 login node name is sometimes ....
+echo "Login to BluePebble ... hopefully this works"
+ssh bp1-login01b.acrc.bris.ac.uk
