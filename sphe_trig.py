7915b7812579d56c9ce85b6470b716c0eabac898
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2020-02-26 15:00:41 +0000

    added a port of J.Wookeys STADIS2. N.B it does not fully agree with the obspy distaz function

--- sphe_trig.py
+++ sphe_trig.py
@@ -9,7 +9,7 @@
 import numpy as np
 
 
-def dist_deg(latA,lonA,latB,lonB):
+def dist_deg(qlat,qlon,slat,slon):
     '''
     Calculates spherical distance between a pair of points
 
@@ -18,10 +18,92 @@ def dist_deg(latA,lonA,latB,lonB):
     Available online at archive.org. Identifier: planeandspherica031803mbp
 
     '''
-    dlon = np.abs(lonA - lonB)
-    sins = np.sin(np.radians(latA))*np.sin(np.radians(latB))
-    coss = np.cos(np.radians(latA))*np.cos(np.radians(latB))*np.cos(np.radians(dlon))
+    dlon = np.abs(qlon - slon)
+    sins = np.sin(np.radians(qlat))*np.sin(np.radians(slat))
+    coss = np.cos(np.radians(qlat))*np.cos(np.radians(slat))*np.cos(np.radians(dlon))
     drad = np.arccos(sins + coss)
     ddeg = np.rad2deg(drad)
 
     return (np.around(ddeg,decimals=3))
+
+
+
+def geocen(arg):
+    '''
+    input:
+    arg    = geographic colatitude (radians)
+  output:
+    geocen = geocentric colatitude (radians)
+    N.B
+    fac = (1-f)**2
+    conversion gc = pi - atan(cos(colat)/f*sin(colat))) is for colatitude (so dont spaz out when it is different
+    from the coversion for latitude you'll find online!)
+    '''
+    fac = 0.993305621334896
+    geocen = np.pi - np.arctan(fac*np.cos(arg)/np.max([1e-30,np.sin(arg)]))
+
+    return geocen
+
+def stadispy(qlat,qlon,slat,slon):
+    '''
+     Computes the epicentral distance and azimuth from source to receiver.
+     Latitudes are converted to geocentric latitudes prior to performing
+     the computations (it is assumed that input latitudes are geographic).
+     Inputs:   qlat  =  quake latitude (degrees)
+               qlon  =  quake longitude (degrees)
+               slat  =  station latitude (degrees)
+               slon  =  station longitude (degrees)
+     Returns:  delta    =  epicentral distance (degrees)
+               az      =  azimuth at quake to receiver, from North (degrees)
+
+     [Ported version of STADIS2 (fortran) written by James Wookey ]
+     Distance is calculated using an implementation of the Vicenty Formula
+    '''
+
+    qcolat = (90 - qlat)
+    if (qlon < 0 ):
+        qlon=qlon+360
+
+    scolat = 90 - slat
+    if (slon < 0 ):
+        slon=slon+360
+
+    print(qlat,qlon,slat,slon)
+    print(qcolat,qlon,scolat,slon)
+    t1deg=scolat
+    p1deg=slon
+    colat=qcolat
+    lon=qlon
+    t1=np.radians(t1deg)
+    p1=np.radians(p1deg)
+    ## For Point A
+    t0 = geocen(np.radians(colat))
+    p0 = np.radians(lon)
+
+    c0 = np.cos(t0)
+    s0 = np.sin(t0)
+    ## For point B
+    t2 = geocen(t1)
+    c1 = np.cos(t2)
+    s1 = np.sin(t2)
+    ## Calculated distance
+    dp = p1-p0
+    co = c0*c1 + s0*s1*np.cos(dp)
+    print(co)
+    si = np.sqrt(1 - co*co)
+    delr = np.arctan2(si,co)
+    delta = np.degrees(delr)
+    #Calculate Azimuth
+    print(c1,c0,co,si,s0)
+    caz=(c1-(c0*co))/(si*s0)
+    if dp != 0:
+        dp2=-dp
+    else:
+        dp2=dp
+    saz=-s1*np.sin(dp2)/si
+    azr=np.arctan2(saz,caz)
+    az = np.degrees(azr)
+    if az < 0 :
+        az = az + 360
+
+    return delta,az
