e73e21dc4cf278e261646b7c2f8a6dfe099c10ea
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2022-08-05 09:12:50 +0100

    Added plotting of secondary solutions

--- plot_slip_planes.py
+++ plot_slip_planes.py
@@ -8,12 +8,18 @@ Created on Wed Mar 31 16:18:00 2021
 import matplotlib.pyplot as plt
 import mplstereonet
 FIG_DIR = '/Users/ja17375/Projects/Epac_fast_anom/Figures/Stereonets'
-def plot_stereonet(model, add_flow=False):
+def plot_stereonet(model, add_flow=False, add_secondaries=False):
     
     fig, ax = mplstereonet.subplots(figsize=(3,3))
+    if add_secondaries:
+        for i in range(0,len(model['a2'])):
+            ax.plane(model['g2'][i]+180, model['a2'][i], linestyle='--', color='darkgrey')
+            ax.line(model['a2'][i], model['g2'][i] - 90, marker='o', color='darkgrey')
+            
     ax.plane(model['gamma']+180, model['alpha'],'k')
-    ax.line(model['alpha'], model['gamma']-90, 'kx')
+    ax.line(model['alpha'], model['gamma']-90, 'ko')
     ax.set_title(model['title']) 
+    
     if add_flow:
         #add forte flow model
         ax.line(5.66, 108.65, 'ro')
@@ -31,11 +37,15 @@ if __name__ == '__main__':
     # ppv001 = {'alpha':22.802, 'gamma':-84.611, 'title':'Post-perovskite [100](001)','file':'ppv001_100'}
     # ppv010 = {'alpha':12.193, 'gamma':93.999, 'title':'Post-perovskite [100](010)','file':'ppv_010_100'}
     
-    ellip = {'alpha':81.6, 'gamma':-140.6, 'title':'Elliptical TI', 'file':'ellipTI'}
-    br = {'alpha':84.4, 'gamma':-144.1, 'title':'Bridgmanite', 'file':'bridgmanite'}
-    ppv001 = {'alpha':27.2, 'gamma':-31.0, 'title':'Post-perovskite [100](001)','file':'ppv001_100'}
-    ppv010 = {'alpha':28.7, 'gamma':-14.6, 'title':'Post-perovskite [100](010)','file':'ppv_010_100'}
+    ellip = {'alpha':81.6, 'gamma':-140.6, 'title':'Elliptical TI', 'file':'ellipTI', 'flag':True,
+             'a2':[90], 'g2':[40]}
+    br = {'alpha':84.4, 'gamma':-144.1, 'title':'Bridgmanite', 'file':'bridgmanite', 'flag':True,
+          'a2':[45], 'g2':[60]}
+    ppv001 = {'alpha':27.2, 'gamma':-31.0, 'title':'Post-perovskite [100](001)','file':'ppv001_100',
+              'flag':True,'a2':[60, 22, 25], 'g2':[160, 100, 150]}
+    ppv010 = {'alpha':28.7, 'gamma':-14.6, 'title':'Post-perovskite [100](010)','file':'ppv_010_100',
+              'flag':True, 'a2':[15], 'g2':[120]}
     for model in [ellip, br, ppv001, ppv010]:
-        plot_stereonet(model)
+        plot_stereonet(model, add_secondaries=model['flag'], add_flow=False)
     # plot_stereonet(ellip)
     
