23202cbf4c37eeefb2be65843939964068f5d4cb
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2022-08-05 09:13:14 +0100

    Moved ftest

--- EnsembleVisualiser.py
+++ EnsembleVisualiser.py
@@ -7,10 +7,9 @@ Created on Wed Nov 18 16:12:12 2020
 """
 import numpy as np
 import pandas as pd
-from scipy import stats
 from matplotlib.gridspec import GridSpec as gs
 import matplotlib.pyplot as plt
-from l2stats import ftest
+from scipy import stats
 FIG_DIR = '/Users/ja17375/Projects/Epac_fast_anom/Figures'
 
 
@@ -33,6 +32,21 @@ def restore_params(param, p_min, p_max):
     true_params = p_min + (p_range * param)
     
     return true_params
+
+def ftest(lam2min,ndf,k=2,alpha=0.05):
+    """
+    returns lambda2 value at 100(1-alpha)% confidence interval
+    by default alpha = 0.05 = 95% confidence interval
+    following Silver and Chan (1991) [modifications by walsh et al., 2013]
+    As we are dealing with traces that have alreayd been passed through SHEBA,
+    we do not need to check (or calculate) degrees of freedom as this has alreay
+    been done.
+
+    Needed for pair_stack to calc lam2alpha for SKS and SKKS
+    """
+    F = stats.f.ppf(1-alpha,k,ndf)
+    lam2alpha = lam2min * ( 1 + (k/(ndf-k)) * F)
+    return lam2alpha
 
 class Ensemble:
     '''
